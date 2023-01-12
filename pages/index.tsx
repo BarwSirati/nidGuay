209bc4dccb5466804df420e622368445c89abd32
BarwSirati <sirati10546@gmail.com>
2023-01-13 02:00:01 +0700

    login

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,17 @@ import { GetServerSideProps } from "next";
 import axios from "axios";
 import { getCookie } from "cookies-next";
 
-const Home = () => {
-  return <div>Home</div>;
+interface Prop {
+  token: string;
+  userId: string;
+}
+const Home: React.FC<Prop> = ({ token, userId }) => {
+  return <div>{userId}</div>;
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const token = getCookie("token", { req, res });
-  if (!token) {
+  const checkToken = getCookie("token", { req, res });
+  if (!checkToken) {
     return {
       redirect: {
         permanent: false,
@@ -17,8 +21,25 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
       props: {},
     };
   }
-  return {
-    props: {},
-  };
+  const token = `Bearer ` + checkToken;
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_BACKEND}/user/current/info`,
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
+  if (response.status !== 200) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+      props: {},
+    };
+  }
+  const userId = response.data.id;
+  return { props: { token, userId } };
 };
 export default Home;
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,47 @@
 import Link from "next/link";
 import Input from "../components/Input";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import axios from "axios";
+import { setCookie } from "cookies-next";
+import { useRouter } from "next/router";
+
+interface FormLogin {
+  username: string;
+  password: string;
+}
+
+const schema = yup.object().shape({
+  username: yup.string().required("กรุณากรอกรหัสนักศึกษา"),
+  password: yup.string().required("กรุณากรอกรหัสผ่าน"),
+});
 
 const Login: React.FC = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormLogin>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data: FormLogin) => {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_BACKEND}/auth/login`,
+      data
+    );
+    if (res.status === 201) {
+      setCookie("token", res.data.access_token[0], {
+        maxAge: 7 * 24 * 60 * 60,
+        path: "/",
+        sameSite: "strict",
+        secure: true,
+      });
+      router.push("/");
+    }
+  };
   return (
     <div className="flex flex-col justify-center items-center px-4 space-y-8 min-h-screen bg-orange-400">
       <h1 className="text-3xl font-semibold">ระบบคิดคำนวณหน่วยกิต</h1>
@@ -27,14 +66,36 @@ const Login: React.FC = () => {
               ลงชื่อเข้าสู่ระบบ
             </div>
           </div>
-          <form className="pt-4 space-y-5 text-lg font-medium">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="pt-4 space-y-5 text-lg font-medium"
+          >
             <div>
               รหัสนักศึกษา
-              <Input type="text" placeholder="รหัสนักศึกษา" />
+              <Input
+                type="text"
+                placeholder="รหัสนักศึกษา"
+                register={register("username", { required: true })}
+              />
+              {errors.username && (
+                <p className="text-base font-semibold text-red-500">
+                  * {errors.username.message}
+                </p>
+              )}
             </div>
+
             <div>
               รหัสผ่าน
-              <Input type="password" placeholder="รหัสผ่าน" />
+              <Input
+                type="password"
+                placeholder="รหัสผ่าน"
+                register={register("password", { required: true })}
+              />
+              {errors.password && (
+                <p className="text-base font-semibold text-red-500">
+                  * {errors.password.message}
+                </p>
+              )}
             </div>
             <button
               type="submit"
