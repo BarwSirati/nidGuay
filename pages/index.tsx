7c5aa3aaddbcb99d148b78b43005e00c6537a52c
BarwSirati <sirati10546@gmail.com>
2023-01-13 01:15:09 +0700

    protected route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,24 @@
+import { GetServerSideProps } from "next";
+import axios from "axios";
+import { getCookie } from "cookies-next";
+
 const Home = () => {
   return <div>Home</div>;
 };
 
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const token = getCookie("token", { req, res });
+  if (!token) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+      props: {},
+    };
+  }
+  return {
+    props: {},
+  };
+};
 export default Home;
