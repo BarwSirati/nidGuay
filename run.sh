404313ecca63761afccbda8bdb2367e9ec490be4
BarwSirati <sirati10546@gmail.com>
2023-01-12 21:39:39 +0700

    update bash script

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 if [ ! "$(docker ps -a -q -f name=nidguay_app_frontend)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=nidguay_app_frontend)" ]; then
-        # cleanup
-        docker-compose up -d --build
-    fi
-    # run your container
     docker-compose up -d
+else
+    docker-compose up -d --build
 fi
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,8 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  darkMode: "class",
+  daisyui: {
+    themes: ["light"],
+  },
 };
