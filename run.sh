944ab500521b966c00326386e098c2bdbf1e9080
BarwSirati <sirati10546@gmail.com>
2023-01-12 16:23:18 +0700

    edit dockerfile

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ ! "$(docker ps -a -q -f name=nextjs)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=nextjs)" ]; then
+if [ ! "$(docker ps -a -q -f name=nidguay_app_frontend)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=nidguay_app_frontend)" ]; then
         # cleanup
         docker-compose up -d --build
     fi
