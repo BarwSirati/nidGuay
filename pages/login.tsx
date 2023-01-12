e17f7aa994e8b8ed833ec993773016c2d17c638e
Bourbxn <jakkapat97@gmail.com>
2023-01-12 16:14:56 +0700

    Comet

---
+++ pages/login.tsx
@@ -0,0 +1,32 @@
+const login = () => {
+  return (
+    <div className="hero min-h-screen bg-[#F7F5EB]">
+      <div className="w-1/2 h-4/5 bg-[#EAE0DA] rounded-xl py-11 px-20">
+        <h1 className="text-5xl pb-14 font-semibold flex justify-center">
+          Sign in
+        </h1>
+        <h2 className="text-2xl font-semibold pb-2">Username</h2>
+        <div className="pb-6">
+          <input
+            type="text"
+            placeholder="Enter username"
+            className="w-full rounded-lg px-4 py-3 focus: outline-[#A0C3D2]"
+          />
+        </div>
+        <h2 className="text-2xl font-semibold pb-2">Password</h2>
+        <div className="pb-10">
+          <input
+            type="text"
+            placeholder="Enter password"
+            className="w-full rounded-lg px-4 py-3 focus: outline-[#A0C3D2]"
+          />
+        </div>
+        <div>
+          <button className="w-full h-16 rounded-lg bg-[#EAC7C7]" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
